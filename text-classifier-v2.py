4b046a2e11f40bd51a8efd8fbff31fa8e5a2a0da
Risang-L <risang.latunra@gmail.com>
2025-05-11 23:06:05 +0800

    Update text-classifier-v2.py

--- text-classifier-v2.py
+++ text-classifier-v2.py
@@ -140,6 +140,3 @@ with col2:
     fig.set_size_inches(4, 3)
     st.pyplot(fig, clear_figure=True, use_container_width=True)
 
-# --- Footer ---
-st.markdown("---")
-st.markdown("<small>Built with ❤️ using Streamlit and SHAP • Thesis project edition</small>", unsafe_allow_html=True)
